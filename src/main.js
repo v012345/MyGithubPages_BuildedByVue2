86b2584595e61bcf86be6951acea7a760f4a8e34
v138468 <v138468@gmail.com>
2021-11-04 09:40:55 +0800

    import element ui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import "./plugins/element.js";
+
 
 Vue.config.productionTip = false
 Vue.use(VueAxios, axios)
